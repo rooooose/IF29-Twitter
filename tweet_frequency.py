17fe97994fcb4905ee0bba72dd85f91ca3e1c8a2
Shir Frank <57755204+rooooose@users.noreply.github.com>
2021-05-08 21:07:22 +0200

    Update tweet_frequency.py

--- tweet_frequency.py
+++ tweet_frequency.py
@@ -22,27 +22,28 @@ r = rethinkDBservice.getConnection()
 
 # np.savetxt('users.txt', users, delimiter=' ', fmt="%s")
 
-data = np.loadtxt('./IF29-Twitter/users.txt', dtype='str', delimiter=' ', ndmin=2)
+data = np.loadtxt('./IF29-Twitter/users.txt', dtype='str', delimiter=' ')
 # shape = data.shape
 # print(shape)
 
-# for x in data:
-    # for i in x:
+for i in data:
 
-cursor_user = r.db('IF29').table('tweets').filter({
+
+    cursor_user = r.db('IF29').table('tweets').filter({
         "user": {
-            "screen_name": "eve1203"
+                "screen_name": i
         }
     }).run()
-print(cursor_user)
+    print(cursor_user)
 
-dates = []
-for t in cursor_user:
+    dates = []
+    for t in cursor_user:
         date = t['created_date']
         date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
         dates.append(date)
         print(date)
-serieDates = pd.Series(dates)
-# on compte le nombre d'occurence pour chaque jour
-frequency = np.mean(serieDates.dt.date.value_counts())
-print(frequency)
+        
+    serieDates = pd.Series(dates)
+    # on compte le nombre d'occurence pour chaque jour
+    frequency = np.mean(serieDates.dt.date.value_counts())
+    print(frequency)
