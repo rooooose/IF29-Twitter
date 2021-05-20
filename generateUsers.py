326cf1756834533b6b5151ed33bcf83b5466053a
Thomas Martignon <thomas.2905@hotmail.fr>
2021-05-20 15:59:49 +0200

    generate user

--- generateUsers.py
+++ generateUsers.py
@@ -2,15 +2,16 @@ from service import rethinkDBservice
 
 r = rethinkDBservice.getConnection()
 
-data = r.db("test").table("tweets").run()
+tweets = r.db("test").table("tweets").run()
 
-for doc in data:
+for tweet in tweets:
     user = {
-        "id": doc["user"]["id"],
-        "name": doc["user"]['name']
+        "id": tweet["user"]["id"],
+        "name": tweet["user"]['name']
     }
-    userTest = r.db("test").table("users").get_all(doc["user"]["id"], index='id').run()
-    if not list(userTest):
+    userTest = r.db("test").table("users").get(789995).run()
+    if not userTest:
         rethinkDBservice.createData('users', user)
+    break
 
     
