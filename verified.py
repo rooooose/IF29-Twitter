41cd9a9105f59598df2d439a50831148ba207f78
drum0006 <maxime.drumel@etudiant.univ-reims.fr>
2021-05-23 20:37:18 +0200

    ajout des scripts visibility et verified

---
+++ verified.py
@@ -0,0 +1,18 @@
+from service import rethinkDBservice
+import pprint
+import json
+r = rethinkDBservice.getConnection()
+
+users = list(rethinkDBservice.getUsersCursors())
+
+def is_verified(user):
+    userId = user["id"]
+    tweets = list(rethinkDBservice.getTweetsByUserIdCursors(userId))
+    verified = False
+    for t in tweets:
+        if t["user"]["verified"]:
+            verified = True
+    return verified
+
+for user in users:
+    rethinkDBservice.updateUser(user["id"],  {"verified" : is_verified(user) } )
---
+++ visibility.py
@@ -0,0 +1,33 @@
+from service import rethinkDBservice
+import pprint
+import json
+r = rethinkDBservice.getConnection()
+
+#Récupération de la liste des utilisateurs
+users = list(rethinkDBservice.getUsersCursors())
+    
+#Calcule la visibilité d'un utilisateur
+def get_visibility(user):
+    userId = user["id"]
+    tweets = list(rethinkDBservice.getTweetsByUserIdCursors(userId))
+
+    costMention = 11.4 #C(@)
+    costHashtag = 11.6 #C(#)
+    
+    nbMention = 0
+    nbHashtag = 0
+
+    for t in tweets:
+        text = t["text"]
+        nbMention += text.count("@")
+        nbHashtag += text.count("#")
+    
+    avgMention = nbMention/len(tweets) * costMention
+    avgHashtag = nbHashtag/len(tweets) * costHashtag
+    visibility = ( avgMention + avgHashtag ) / 280
+
+    return visibility
+
+for user in users:
+    rethinkDBservice.updateUser(user["id"],  {"visibility" : get_visibility(user) } )
+    #print(user["id"],get_visibility(user))
