1eb9239823f4dcafafb4a61822edbe23d0ba2073
BertilleT <bertille.temple_boyer__dury@utt.fr>
2021-05-27 17:31:38 +0200

    update mediumLength

---
+++ mediumLengthTweet.py
@@ -0,0 +1,27 @@
+from service import rethinkDBservice
+import pprint
+import json
+
+
+r = rethinkDBservice.getConnection()
+users = rethinkDBservice.getUsersCursors()
+
+def getLength(t):
+        l = len(t['text'])
+        return l
+
+def get_users_mediumLengthTweets(u):
+
+    tweets = rethinkDBservice.getTweetsByUserIdCursors(u["id"])
+
+    lengths = [getLength(t) for t in tweets]
+    somme=0
+    for l in lengths:
+       somme = somme + l
+    return somme
+    nbTweets = len(tweets)
+    mediumLength = somme/nbTweets
+    
+    rethinkDBservice.updateUser(u["id"], {"medium_length_by_tweet": mediumLength})
+
+L = [get_users_mediumLengthTweets(u) for u in users]
