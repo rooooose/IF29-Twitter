12b9b67478707a88dabc496e0d9e787d969c4e3a
Marti <thomas.2905@hotmail.fr>
2021-06-03 14:56:34 +0200

    fix main

--- mainDataCalc.py
+++ mainDataCalc.py
@@ -14,26 +14,27 @@ import json
 users = rethinkDBservice.getUsersCursors()
 cmp = 0
 for user in users:
-    tweets = list(rethinkDBservice.getTweetsByUserIdCursors(user["id"]))
+    if len(user) <= 1 :
+        tweets = list(rethinkDBservice.getTweetsByUserIdCursors(user["id"]))
 
-    if len(tweets) != 0:
-        res = {
-            "agressivite": agressiviteTweet.calcAgressiviteTweet(tweets),
-            "avg_retweet": avgRetweetUrlHashtag.getRetweetAvg(tweets),
-            "avg_url": avgRetweetUrlHashtag.getURLAvg(tweets),
-            "avg_hashtag": avgRetweetUrlHashtag.getHashtagAvg(tweets),
-            "rationFollowersFriends": followers.calcFollowers(tweets),
-            "mediumLength": mediumLengthTweet.get_users_mediumLengthTweets(tweets),
-            "rateOfRepliedTweets": repliedTweets.calcRateRepliedTweets(tweets),
-            "tweet_per_day": tweet_frequency.calcTweetFrequency(tweets),
-            "verified": verified.calcVerified(tweets),
-            "visibility": visibility.calcVisibility(tweets),
-            "version": "1"
-        }
-        rethinkDBservice.updateUser(user["id"], res)
-        # pprint.pprint(res)
-        # pprint.pprint(user)
-        cmp+=1
-        print(cmp)
-        # if cmp > 10:
-        #     break
+        if len(tweets) != 0:
+            res = {
+                "agressivite": agressiviteTweet.calcAgressiviteTweet(tweets),
+                "avg_retweet": avgRetweetUrlHashtag.getRetweetAvg(tweets),
+                "avg_url": avgRetweetUrlHashtag.getURLAvg(tweets),
+                "avg_hashtag": avgRetweetUrlHashtag.getHashtagAvg(tweets),
+                "rationFollowersFriends": followers.calcFollowers(tweets),
+                "mediumLength": mediumLengthTweet.get_users_mediumLengthTweets(tweets),
+                "rateOfRepliedTweets": repliedTweets.calcRateRepliedTweets(tweets),
+                "tweet_per_day": tweet_frequency.calcTweetFrequency(tweets),
+                "verified": verified.calcVerified(tweets),
+                "visibility": visibility.calcVisibility(tweets),
+                "version": "1"
+            }
+            rethinkDBservice.updateUser(user["id"], res)
+            # pprint.pprint(res)
+            # pprint.pprint(user)
+            cmp+=1
+            print(cmp)
+            # if cmp > 1:
+            #     break
